283439789db9ad757d9df369fb4758f13f4b2ea9
Siddharth <112471142+Siddharth6527@users.noreply.github.com>
2023-08-17 12:00:13 +0530

    Implemented better Algorithm

--- src/components/DayCard.js
+++ src/components/DayCard.js
@@ -3,10 +3,78 @@ import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import DateHeader from "./DateHeader";
 import TasksList from "./TasksList";
-import TasksList2 from "./TasksList2";
 import { Divider, Grid } from "@mui/material";
 
 export default function DayCard() {
+  const data1 = [
+    {
+      id: "1",
+      icon: "1",
+      time: "11:30 AM",
+      task: "New Task Assigned to you",
+      author: "Sheila O'Riley",
+    },
+    {
+      id: "2",
+      icon: "2",
+      time: "11:30 AM",
+      task: "New feedback received for Task Name",
+      author: "Sheila O'Riley",
+    },
+    {
+      id: "3",
+      icon: "3",
+      time: "11:30 AM",
+      task: "Task marked completed by you",
+      author: "Sheila O'Riley",
+    },
+    {
+      id: "4",
+      icon: "4",
+      time: "11:30 AM",
+      task: "Task Name updated for journey Journey Name",
+      author: "Sheila O'Riley",
+    },
+    {
+      id: "5",
+      icon: "5",
+      time: "11:30 AM",
+      task: "Follow up on feedback for Task Name completed on Date XX/YY/ZZ",
+      author: "Sheila O'Riley",
+    },
+  ];
+
+  const data2 = [
+    {
+      id: "6",
+      icon: "6",
+      time: "11:30 AM",
+      task: "Meeting with Bobby Charlie",
+      author: "Sheila O'Riley",
+    },
+    {
+      id: "7",
+      icon: "7",
+      time: "11:30 AM",
+      task: "Chat about Design Mentorship",
+      author: "Sheila O'Riley",
+    },
+    {
+      id: "8",
+      icon: "8",
+      time: "11:30 AM",
+      task: "Critical delay observed in time line adherence",
+      author: "Sheila O'Riley",
+    },
+    {
+      id: "9",
+      icon: "9",
+      time: "11:30 AM",
+      task: "Finish project documentaton for review (1-1 checkin)",
+      author: "Sheila O'Riley",
+    },
+  ];
+
   return (
     <>
       {/* Day Card 1 */}
@@ -22,7 +90,8 @@ export default function DayCard() {
         <CardContent>
           {/* // Passing Props for keeping code lean */}
           <DateHeader day="WED" date="30" month="Jul" />
-          <TasksList />
+          {/* NEW CODE */}
+          <TasksList data={data1} />
         </CardContent>
       </Card>
       {/* Added Divider */}
@@ -60,7 +129,8 @@ export default function DayCard() {
         <CardContent>
           {/* // Passing Props for keeping code lean */}
           <DateHeader day="THU" date="29" month="Jul" />
-          <TasksList2 />
+          {/* NEW CODE */}
+          <TasksList data={data2} />
         </CardContent>
       </Card>
     </>
--- src/components/TasksList.js
+++ src/components/TasksList.js
@@ -1,11 +1,14 @@
 import * as React from "react";
 import { styled } from "@mui/material/styles";
-import Box from "@mui/material/Box";
-import List from "@mui/material/List";
-import ListItem from "@mui/material/ListItem";
-import Grid from "@mui/material/Grid";
-import Typography from "@mui/material/Typography";
-import { Stack, Divider } from "@mui/material";
+import {
+  Box,
+  List,
+  ListItem,
+  Grid,
+  Typography,
+  Stack,
+  Divider,
+} from "@mui/material";
 import AuthorIMG from "../Photo.svg";
 import SwitchAccountIcon from "@mui/icons-material/SwitchAccount";
 import ChatOutlinedIcon from "@mui/icons-material/ChatOutlined";
@@ -13,12 +16,16 @@ import DoneOutlinedIcon from "@mui/icons-material/DoneOutlined";
 import BrowserUpdatedOutlinedIcon from "@mui/icons-material/BrowserUpdatedOutlined";
 import BookmarkIcon from "@mui/icons-material/Bookmark";
 import CustomDivider from "../components/CustomDivider";
+import GroupsIcon from "@mui/icons-material/Groups";
+import ChatIcon from "@mui/icons-material/Chat";
+import PriorityHighIcon from "@mui/icons-material/PriorityHigh";
+import QueryBuilderIcon from "@mui/icons-material/QueryBuilder";
 
 const Demo = styled("div")(({ theme }) => ({
   backgroundColor: theme.palette.background.paper,
 }));
 
-export default function TasksList() {
+export default function TasksList(props) {
   // List of Events
   return (
     <Box sx={{ flexGrow: 1, maxWidth: 752 }}>
@@ -26,222 +33,184 @@ export default function TasksList() {
         <Grid item xs={12}>
           <Demo>
             <List sx={{ background: "#f8f9fa" }} dense={true}>
-              <ListItem>
-                <Stack
-                  direction="row"
-                  spacing={3}
-                  sx={{ display: "flex", alignItems: "center" }}
-                >
-                  <Typography variant="Body1" color="#adb5bd">
-                    11:30 AM
-                  </Typography>
-                  <Box>
-                    <Divider
-                      sx={{
-                        height: 37 + "px",
-                        position: "absolute",
-                        left: 17 + "%",
-                        top: -20 + "px",
-                        zIndex: "8",
-                      }}
-                      orientation="vertical"
-                    />
-                    <CustomDivider />
-                    <SwitchAccountIcon
-                      sx={{
-                        color: "white",
-                        background: "#4c6ef5",
-                        padding: 8 + "px",
-                        borderRadius: 50 + "%",
-                      }}
-                    />
-                  </Box>
-                  <Box>
-                    <Typography
-                      sx={{ mt: 20 + "px" }}
-                      variant="subtitle1"
-                      color="#3FB883"
-                    >
-                      New task assigned to you
+              {/* Mapping through the Array of elements passed as Props */}
+              {props.data.map((item) => (
+                <ListItem>
+                  <Stack
+                    direction="row"
+                    spacing={3}
+                    sx={{ display: "flex", alignItems: "center" }}
+                  >
+                    <Typography variant="Body1" color="#adb5bd">
+                      {item.time}
                     </Typography>
+                    {item.icon === "1" && (
+                      <Box>
+                        <Divider
+                          sx={{
+                            height: 37 + "px",
+                            position: "absolute",
+                            left: 17 + "%",
+                            top: -20 + "px",
+                            zIndex: "8",
+                          }}
+                          orientation="vertical"
+                        />
+                        <CustomDivider />
+                        <SwitchAccountIcon
+                          sx={{
+                            color: "white",
+                            background: "#4c6ef5",
+                            padding: 8 + "px",
+                            borderRadius: 50 + "%",
+                          }}
+                        />
+                      </Box>
+                    )}
+
+                    {item.icon === "2" && (
+                      <Box>
+                        <CustomDivider />
+                        <ChatOutlinedIcon
+                          sx={{
+                            color: "white",
+                            background: "#DC395F",
+                            padding: 8 + "px",
+                            borderRadius: 50 + "%",
+                          }}
+                        />
+                      </Box>
+                    )}
+                    {item.icon === "3" && (
+                      <Box>
+                        <CustomDivider />
+                        <DoneOutlinedIcon
+                          sx={{
+                            color: "white",
+                            background: " #3FB883",
+                            padding: 8 + "px",
+                            borderRadius: 50 + "%",
+                          }}
+                        />
+                      </Box>
+                    )}
+                    {item.icon === "4" && (
+                      <Box>
+                        <CustomDivider />
+                        <BrowserUpdatedOutlinedIcon
+                          sx={{
+                            color: "white",
+                            background: "#FB8789",
+                            padding: 8 + "px",
+                            borderRadius: 50 + "%",
+                            zIndex: "10",
+                          }}
+                        />
+                      </Box>
+                    )}
+                    {item.icon === "5" && (
+                      <Box>
+                        <Divider
+                          sx={{
+                            height: 100 + "px",
+                            position: "absolute",
+                            left: 17 + "%",
+                            top: 73 + "%",
+                            zIndex: "5",
+                          }}
+                          orientation="vertical"
+                        />
+                        <BookmarkIcon
+                          sx={{
+                            color: "white",
+                            background: "#F2BB27",
+                            padding: 8 + "px",
+                            borderRadius: 50 + "%",
+                          }}
+                        />
+                      </Box>
+                    )}
+                    {item.icon === "6" && (
+                      <Box>
+                        <Divider
+                          sx={{
+                            height: 37 + "px",
+                            position: "absolute",
+                            left: 17 + "%",
+                            top: -20 + "px",
+                            zIndex: "8",
+                          }}
+                          orientation="vertical"
+                        />
+                        <CustomDivider />
+                        <GroupsIcon
+                          sx={{
+                            color: "white",
+                            background: "#4c6ef5",
+                            padding: 8 + "px",
+                            borderRadius: 50 + "%",
+                          }}
+                        />
+                      </Box>
+                    )}
+                    {item.icon === "7" && (
+                      <Box>
+                        <CustomDivider />
+                        <ChatIcon
+                          sx={{
+                            color: "white",
+                            background: "#A259FF",
+                            padding: 8 + "px",
+                            borderRadius: 50 + "%",
+                          }}
+                        />
+                      </Box>
+                    )}
+                    {item.icon === "8" && (
+                      <Box>
+                        <CustomDivider />
+                        <PriorityHighIcon
+                          sx={{
+                            color: "white",
+                            background: " #E94235",
+                            padding: 8 + "px",
+                            borderRadius: 50 + "%",
+                          }}
+                        />
+                      </Box>
+                    )}
+                    {item.icon === "9" && (
+                      <Box>
+                        <QueryBuilderIcon
+                          sx={{
+                            color: "white",
+                            background: "#F2DF37",
+                            padding: 8 + "px",
+                            borderRadius: 50 + "%",
+                          }}
+                        />
+                      </Box>
+                    )}
                     <Box>
-                      <Box sx={{ display: "flex", gap: 5 + "px" }}>
-                        <img src={AuthorIMG} alt="" />
-                        <Typography variant="subtitle2" color="#adb5bd">
-                          Sheila O'Riley
-                        </Typography>
+                      <Typography
+                        sx={{ mt: 20 + "px" }}
+                        variant="subtitle1"
+                        color="initial"
+                      >
+                        {item.task}
+                      </Typography>
+                      <Box>
+                        <Box sx={{ display: "flex", gap: 5 + "px" }}>
+                          <img src={AuthorIMG} alt="" />
+                          <Typography variant="subtitle2" color="#adb5bd">
+                            {item.author}
+                          </Typography>
+                        </Box>
                       </Box>
                     </Box>
-                  </Box>
-                </Stack>
-              </ListItem>
-              <ListItem>
-                <Stack
-                  direction="row"
-                  spacing={3}
-                  sx={{ display: "flex", alignItems: "center" }}
-                >
-                  <Typography variant="Body1" color="#adb5bd">
-                    11:30 AM
-                  </Typography>
-                  <Box>
-                    <CustomDivider />
-                    <ChatOutlinedIcon
-                      sx={{
-                        color: "white",
-                        background: "#DC395F",
-                        padding: 8 + "px",
-                        borderRadius: 50 + "%",
-                      }}
-                    />
-                  </Box>
-                  <Box>
-                    <Typography
-                      sx={{ mt: 20 + "px" }}
-                      variant="subtitle1"
-                      color="initial"
-                    >
-                      New feedback recieved for Task Name
-                    </Typography>
-                    <Box>
-                      <Box sx={{ display: "flex", gap: 5 + "px" }}>
-                        <img src={AuthorIMG} alt="" />
-                        <Typography variant="subtitle2" color="#adb5bd">
-                          Sheila O'Riley
-                        </Typography>
-                      </Box>
-                    </Box>
-                  </Box>
-                </Stack>
-              </ListItem>
-              <ListItem>
-                <Stack
-                  direction="row"
-                  spacing={3}
-                  sx={{ display: "flex", alignItems: "center" }}
-                >
-                  <Typography variant="Body1" color="#adb5bd">
-                    11:30 AM
-                  </Typography>
-                  <Box>
-                    <CustomDivider />
-                    <DoneOutlinedIcon
-                      sx={{
-                        color: "white",
-                        background: " #3FB883",
-                        padding: 8 + "px",
-                        borderRadius: 50 + "%",
-                      }}
-                    />
-                  </Box>
-                  <Box>
-                    <Typography
-                      sx={{ mt: 20 + "px" }}
-                      variant="subtitle1"
-                      color="initial"
-                    >
-                      Task marked completed by you
-                    </Typography>
-                    <Box>
-                      <Box sx={{ display: "flex", gap: 5 + "px" }}>
-                        <img src={AuthorIMG} alt="" />
-                        <Typography variant="subtitle2" color="#adb5bd">
-                          Sheila O'Riley
-                        </Typography>
-                      </Box>
-                    </Box>
-                  </Box>
-                </Stack>
-              </ListItem>
-              <ListItem>
-                <Stack
-                  direction="row"
-                  spacing={3}
-                  sx={{ display: "flex", alignItems: "center" }}
-                >
-                  <Typography variant="Body1" color="#adb5bd">
-                    11:30 AM
-                  </Typography>
-                  <Box>
-                    <CustomDivider />
-                    <BrowserUpdatedOutlinedIcon
-                      sx={{
-                        color: "white",
-                        background: "#FB8789",
-                        padding: 8 + "px",
-                        borderRadius: 50 + "%",
-                        zIndex: "10",
-                      }}
-                    />
-                  </Box>
-                  <Box>
-                    <Typography
-                      sx={{ mt: 20 + "px" }}
-                      variant="subtitle1"
-                      color="initial"
-                    >
-                      Task Name updated for journey Journey Name
-                    </Typography>
-                    <Box>
-                      <Box sx={{ display: "flex", gap: 5 + "px" }}>
-                        <img src={AuthorIMG} alt="" />
-                        <Typography variant="subtitle2" color="#adb5bd">
-                          Sheila O'Riley
-                        </Typography>
-                      </Box>
-                    </Box>
-                  </Box>
-                </Stack>
-              </ListItem>
-              <ListItem>
-                <Stack
-                  direction="row"
-                  spacing={3}
-                  sx={{ display: "flex", alignItems: "center" }}
-                >
-                  <Typography variant="Body1" color="#adb5bd">
-                    11:30 AM
-                  </Typography>
-                  <Box>
-                    <Divider
-                      sx={{
-                        height: 100 + "px",
-                        position: "absolute",
-                        left: 17 + "%",
-                        top: 73 + "%",
-                        zIndex: "5",
-                      }}
-                      orientation="vertical"
-                    />
-                    <BookmarkIcon
-                      sx={{
-                        color: "white",
-                        background: "#F2BB27",
-                        padding: 8 + "px",
-                        borderRadius: 50 + "%",
-                      }}
-                    />
-                  </Box>
-                  <Box>
-                    <Typography
-                      sx={{ mt: 20 + "px" }}
-                      variant="subtitle1"
-                      color="initial"
-                    >
-                      Follow up on feedback for Task Name completed on Date
-                      XX/YY/ZZZZ
-                    </Typography>
-                    <Box>
-                      <Box sx={{ display: "flex", gap: 5 + "px" }}>
-                        <img src={AuthorIMG} alt="" />
-                        <Typography variant="subtitle2" color="#adb5bd">
-                          Sheila O'Riley
-                        </Typography>
-                      </Box>
-                    </Box>
-                  </Box>
-                </Stack>
-              </ListItem>
+                  </Stack>
+                </ListItem>
+              ))}
             </List>
           </Demo>
         </Grid>
